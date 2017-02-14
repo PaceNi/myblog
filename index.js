67288ae27ecd0a1a973e4b990e0d69b0000827cc
Pace <pace@paceni.local>
2017-02-14 18:18:36 +0800

    使用ejs渲染前端

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ var app = express();
 // 设置模板目录
 app.set('views', path.join(__dirname, 'views'));
 // 设置模板引擎为 ejs
-app.set('view engine', 'jsx');
-app.engine('jsx', reactViews.createEngine());
+app.set('view engine', 'ejs');
+// app.engine('jsx', reactViews.createEngine());
 
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
